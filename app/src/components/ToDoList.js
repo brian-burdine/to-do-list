cf67ab68fe8e79d6af3805cb7b792b1cf4da8577
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-03-06 19:26:03 +0000

    Formatted ToDoList.js to be more in line with style guide

--- app/src/components/ToDoList.js
+++ app/src/components/ToDoList.js
@@ -26,30 +26,56 @@ function ToDoList () {
         <div className="row">
             <div className="col text-center">
                 <table className="table table-hover" id="to-do-list">
-                    <InputField tasks={tasks} setTasks={setTasks} />
+                    <InputField 
+                        tasks={tasks} 
+                        setTasks={setTasks} 
+                    />
                     <tbody className="table-group-divider">
                         {
                             (currentView === 'all' && tasks
                             .map((task, index) => {
-                                return <ToDoItem key={task.id} tasks={tasks} 
-                                setTasks={setTasks} index={index} />
+                                return (
+                                    <ToDoItem 
+                                        key={task.id} 
+                                        tasks={tasks} 
+                                        setTasks={setTasks} 
+                                        index={index} 
+                                    />
+                                )
                             })) || 
                             (currentView === 'active' && tasks
                             .filter(task => !task.done)
                             .map((task, index, newTasks) => {
-                                return <ToDoItem key={task.id} tasks={newTasks} 
-                                setTasks={setTasks} index={index} />
+                                return (
+                                    <ToDoItem 
+                                        key={task.id} 
+                                        tasks={newTasks} 
+                                        setTasks={setTasks} 
+                                        index={index} 
+                                    />
+                                )
                             })) || 
                             (currentView === 'completed' && tasks
                             .filter(task => task.done)
                             .map((task, index, newTasks) => {
-                                return <ToDoItem key={task.id} tasks={newTasks} 
-                                setTasks={setTasks} index={index} />
+                                return (
+                                    <ToDoItem 
+                                        key={task.id} 
+                                        tasks={newTasks} 
+                                        setTasks={setTasks} 
+                                        index={index} 
+                                    />
+                                )
                             }))
                         }
                     </tbody>
-                    {tasks.length > 0 && <Footer tasks={tasks} 
-                    setTasks={setTasks} setCurrentView={setCurrentView} />}
+                    {tasks.length > 0 && 
+                        <Footer 
+                            tasks={tasks} 
+                            setTasks={setTasks} 
+                            setCurrentView={setCurrentView} 
+                        />
+                    }
                 </table>
             </div>
         </div>
